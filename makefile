a1a0161a0d2f401d5117c6fb690a2d9ad4f3657d
VermillionAzure <aytasato@gmail.com>
2016-09-12 19:40:19 -1000

    Successfully deliminated input according to pipe symbol and spaces and placed into exec_info structs.

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-all: sshell.c
+all: sshell.c sshell2.c
 	gcc sshell.c -o sshell
+	gcc sshell2.c -o sshell2
 
-debug: sshell.c
-	gcc -DDEBUG sshell.c -o debug-sshell
+debug: sshell.c sshell2.c
+	gcc -DDEBUG -g sshell.c -o debug-sshell
+	gcc -DDEBUG -g sshell2.c -o debug-sshell2
---
+++ test.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * Outputs an array of structs ready to be parsed into exec()
+ *         as well as the number of arguments into num_cmd
+ */
+struct exec_info* parse_input(int argc,
+                              char** argv,
+                              size_t maxstrlen,
+                              size_t* num_cmd) {
+    
+}
+
+int main(int argc, char** argv) {
+    
+}
